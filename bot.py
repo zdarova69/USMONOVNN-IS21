68665ae28150dfd92322e43667554046e103181b
zdarova69 <113101818+zdarova69@users.noreply.github.com>
2023-11-25 14:46:54 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,103 @@
+#импортируем библиотеки
+import telebot
+from telebot import types
+
+import json
+import time
+
+import testAPI2
+
+
+
+
+class MyBot:
+    def __init__(self):
+        #данные бота
+        self.token = "6095239590:AAGLodvJdqeE7EK-dfiFtZgsr8WtSwwd1-4"
+        self.bot = telebot.TeleBot(self.token)
+        #получаем имя пользователя
+        self.user_name = ''
+        self.chat_id = ''
+
+        #кнопки для ответов на вопросы
+        self.markup1 = types.InlineKeyboardMarkup(row_width=2)
+        self.item0 = types.InlineKeyboardButton(text="что такое переменная?", callback_data="переменная")
+        self.item1 = types.InlineKeyboardButton(text="что такое типы данных?", callback_data="типы данных")
+        self.item2 = types.InlineKeyboardButton(text="что такое условия?", callback_data="условия")
+        self.item3 = types.InlineKeyboardButton(text="что такое циклы?", callback_data="циклы")
+        self.item4 = types.InlineKeyboardButton(text="что такое ооп?", callback_data="ооп")
+        
+        self.kb = self.markup1.add(self.item0, self.item1, self.item2, self.item3, self.item4)
+        
+        self.markup_search = types.InlineKeyboardMarkup(row_width=1)
+        self.item_result = types.InlineKeyboardButton(text="получить ответ", callback_data="ответ")
+        self.kb_search = self.markup_search.add(self.item_result)
+    
+    #запускаем бота
+    def run(self):
+        print('бот запущен')
+        #команда для запуска бота
+        @self.bot.message_handler(commands=['start'])
+        def welcome(message):
+            #задаем параметры переменным
+            self.chat_id = message.chat.id
+            self.user_name = message.from_user.first_name
+            #команда для приветствия
+            self.bot.reply_to(message, f"Привет, {self.user_name}! Я бот кода будущего 🤡. Задай мне вопрос: \n Если хочешь что-то найти при помощи меня,напиши search и предмет поиска \n Пример: /search шрек", reply_markup=self.kb)
+        #команда для поиска
+        @self.bot.message_handler(commands=['search'])
+        def search(message):
+            request = message.text[8:]
+            print(message.chat.id)
+            self.chat_id = message.chat.id
+            self.bot.send_message(message.chat.id, text='Бот начал поиск!')
+            testAPI2.search_api(request)
+            time.sleep(1)
+            self.bot.send_message(message.chat.id, text='поиск закончен!', reply_markup=self.kb_search)
+
+            
+            
+            
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def callback_inline(call):
+            if call.data == "переменная":
+                markup = types.InlineKeyboardMarkup(row_width=2)
+                markup.add(self.item1, self.item2, self.item3, self.item4)
+                self.bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
+                self.bot.send_message(call.message.chat.id, "Переме́нная в императивном программировании — поименованная, либо адресуемая иным способом область памяти, адрес которой можно использовать для осуществления доступа к данным. Данные, находящиеся в переменной, называются значением этой переменной.", reply_markup=markup)
+
+            elif call.data == "типы данных":
+                markup = types.InlineKeyboardMarkup(row_width=2)
+                markup.add(self.item0, self.item2, self.item3, self.item4)
+                self.bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
+                self.bot.send_message(call.message.chat.id, "Типы данных - класс данных, характеризуемый членами класса и операциями, которые могут быть к ним применены", reply_markup=markup)
+
+            elif call.data == "условия":
+                markup = types.InlineKeyboardMarkup(row_width=2)
+                markup.add(self.item0, self.item1, self.item3, self.item4)
+                self.bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
+                self.bot.send_message(call.message.chat.id, "Условия - выражение логического типа (Boolean), которое может принимать одно из двух значений: True (истина) или False (ложь)", reply_markup=markup)
+
+            elif call.data == "циклы":
+                markup = types.InlineKeyboardMarkup(row_width=2)
+                markup.add(self.item0, self.item1, self.item2, self.item4)
+                self.bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
+                self.bot.send_message(call.message.chat.id, "Циклы - разновидность управляющей конструкции в высокоуровневых языках программирования, предназначенная для организации многократного исполнения набора инструкций.", reply_markup=markup)
+
+            elif call.data == "ооп":
+                markup = types.InlineKeyboardMarkup(row_width=2)
+                markup.add(self.item0, self.item1, self.item2, self.item3)
+                self.bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
+                self.bot.send_message(call.message.chat.id, "Объе́ктно-ориенти́рованное программи́рование — методология программирования, основанная на представлении программы в виде совокупности объектов, каждый из которых является экземпляром определённого класса, а классы образуют иерархию наследования.", reply_markup=markup)
+            
+            elif call.data == 'ответ':
+                print('Человек получил ответ')
+                with open('result.json', 'r') as result:
+                    result = json.load(result)
+                    self.bot.send_message(self.chat_id, text=result)
+                    
+        self.bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    my_bot = MyBot()
+    my_bot.run()
---
+++ testAPI2.py
@@ -0,0 +1,35 @@
+from serpapi import GoogleSearch
+import os, json
+
+def search_api(a):
+  print(f'получено {a}')
+  params = {
+    "api_key": "35ad8f3f0a6df240bab8ad52165ba01f5c004d1a60b70c8552cb0e9c236265ff",
+    "engine": "google",
+    "q": a,
+    "tbm": "isch",
+    "num": "100",
+    'ijn': 0
+  }
+
+  search = GoogleSearch(params)
+  image_result_json = []
+  image_in_present = True
+  while image_in_present:
+    results = search.get_dict()
+
+    if 'error' not in results:
+      for image in results['images_results']:
+        if image['original'] not in image_result_json:
+          image_result_json.append(image['original'])
+          print(len(image_result_json), image['original'])
+      params["ijn"] = +1
+    else:
+      image_in_present = False
+      print(results['error'])
+    if len(image_result_json)  > 100:
+      len(image_result_json)
+      break
+
+  with open('result.json', 'w') as file:
+    json.dump(image_result_json, file)
